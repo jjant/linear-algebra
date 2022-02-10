e9363b79c08d79595cf3487049b897f32fc8acfe
jjant <jantonielli@itba.edu.ar>
2022-02-10 01:24:19 +0000

    Add Vec4 tests and add some functions

--- src/Vec4.elm
+++ src/Vec4.elm
@@ -1,6 +1,8 @@
 module Vec4 exposing
     ( Vec4
     , vec4
+    , toString
+    , setW, setX, setY, setZ
     )
 
 {-| Vec4
@@ -11,6 +13,8 @@ module Vec4 exposing
 # Create
 
 @docs vec4
+
+@docs toString
 
 -}
 
@@ -29,3 +33,45 @@ type alias Vec4 =
 vec4 : Float -> Float -> Float -> Float -> Vec4
 vec4 =
     Vec4
+
+
+
+---- Setters ----
+
+
+{-| -}
+setX : Float -> Vec4 -> Vec4
+setX x vec =
+    { vec | x = x }
+
+
+{-| -}
+setY : Float -> Vec4 -> Vec4
+setY y vec =
+    { vec | y = y }
+
+
+{-| -}
+setZ : Float -> Vec4 -> Vec4
+setZ z vec =
+    { vec | z = z }
+
+
+{-| -}
+setW : Float -> Vec4 -> Vec4
+setW w vec =
+    { vec | w = w }
+
+
+toString : Vec4 -> String
+toString { x, y } =
+    "{ "
+        ++ "x = "
+        ++ String.fromFloat x
+        ++ ", y = "
+        ++ String.fromFloat y
+        ++ ", z = "
+        ++ String.fromFloat z
+        ++ ", w = "
+        ++ String.fromFloat w
+        ++ " }"
